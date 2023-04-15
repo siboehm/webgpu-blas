0de6cd63a1eeff7d29321533c09f94f40cd49f0d
Simon Boehm <simon@siboehm.com>
2023-04-15 16:39:06 -0700

    support beta in fallback

--- src/sgemm_fallback.ts
+++ src/sgemm_fallback.ts
@@ -1,9 +1,6 @@
 
 export function sgemm(m: number, n: number, k: number, alpha: number, a: Float32Array, b: Float32Array, beta: number = 0.0, c?: Float32Array): Float32Array {
   // To improve performance on WebGPU unsupported devices, use WebGL or WebAssembly
-  if (beta !== 0.0) {
-    throw new Error('beta !== 0.0 is not yet supported');
-  }
   const result = new Float32Array(m * n);
   for (let row = 0; row < m; row++) {
     for (let col = 0; col < n; col++) {
@@ -11,7 +8,7 @@ export function sgemm(m: number, n: number, k: number, alpha: number, a: Float32
       for (let j = 0; j < k; j++) {
         sum += a[row * k + j] * b[j * n + col];
       }
-      result[row * n + col] = sum * alpha;
+      result[row * n + col] = alpha * sum + (c ? beta * c[row * n + col] : 0.0);
     }
   }
   return result;
